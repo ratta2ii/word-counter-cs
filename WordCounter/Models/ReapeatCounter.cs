22c47132fb1cf10a8eb659f0559c0691840c4430
Trevor Mackin <tismetrm@gmail.com>
2019-12-20 10:22:53 -0800

    Add both a Word field, and a Sentence field to the Count class. Since we will using auto implemented properties, add both a getter and setter to the variable declaration. Add a Count constructor, that takes in both a word and sentence strings.

--- WordCounter/Models/ReapeatCounter.cs
+++ WordCounter/Models/ReapeatCounter.cs
@@ -4,6 +4,14 @@ namespace WordCounter.Models
 {
     public class Count
     {
+        string Word { get; set; }
+        string Sentence { get; set; }
+
+        public Count(string word, string sentence)
+        {
+            Word = word; 
+            Sentence = sentence;
+        }
         
     }
 }
