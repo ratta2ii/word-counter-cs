b66f8fc80b378a5fa30da181100675385b3d1281
Trevor Mackin <tismetrm@gmail.com>
2019-12-20 13:18:09 -0800

    Work on the logic behind removing all special characters before moving on to the CountWords method.

--- WordCounter/Models/ReapeatCounter.cs
+++ WordCounter/Models/ReapeatCounter.cs
@@ -6,12 +6,44 @@ namespace WordCounter.Models
     {
         public string Word { get; set; }
         public string Sentence { get; set; }
+        public int Counter { get; set; }
+        
 
         public Count(string word, string sentence)
         {
             Word = word; 
             Sentence = sentence;
+            Counter = 0;
         }
+
+        // public string CountWords()
+        // {
+        //    char[] charsToTrim = {'.', '?', '!', ';', ':'};
+        //    string result = Sentence.Trim(charsToTrim);
+        //    result = result.ToLower();
+
+        //    return result;
+        // }
+
+
+        public static string RemoveSpecialChars(string sentence)
+        {
+            string sentToLower = sentence.ToLower();
+            string sentenceCleaned = "";
+            char[] sentenceArr = sentToLower.ToCharArray();
+            foreach(char c in sentenceArr)
+            {
+                if ((c >= 'a' && c <= 'z') || c == ' ')
+                {
+                    sentenceCleaned += c;
+                }
+            }
+            return sentenceCleaned;
+        }
+
+
+      
+
         
     }
 }
