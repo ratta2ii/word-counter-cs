8eebe291abbdcdab00926f305c4e478629f86399
Trevor Mackin <tismetrm@gmail.com>
2019-12-20 08:45:28 -0800

    Initialize project WordCounter. Set up file structure to test with MSTest. Add boilerplates to files ..csproj. Create a Program.cs file with a Main method for the application output. Create a class named RepeatCounter, and add this class to a namespace called RepeatCounter.Models.

---
+++ WordCounter/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RepeatCounter.Models
+{
+    public class Program 
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
