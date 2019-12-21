e9890a43f19d8fa1a49f60c17a1febb82461e66b
Trevor Mackin <tismetrm@gmail.com>
2019-12-20 16:27:41 -0800

    Add additional unit test to test for different outcomes.

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -18,18 +18,38 @@ namespace WordCounter.Tests
             Assert.AreEqual(typeof(Count), rcTest.GetType());
         }
 
-
         [TestMethod]
 
         public void CountWords_CountsWordsInSentence_1()
         {
-            string word = "running";
-            string sentence = "I want to go running in the moring";
+            string word = "dog";
+            string sentence = "It truly does make sense that a dog has been called mans best friend.";
             Count testInstance = new Count(word, sentence);
             testInstance.CountWords();
             Assert.AreEqual(1, testInstance.Counter);
         }
 
+        [TestMethod]
+
+        public void CountWords_CountsWordsInSentence_2()
+        {
+            string word = "beach";
+            string sentence = "The beach has always been a part of my life, as I have been going to the beach since early childhood.";
+            Count testInstance = new Count(word, sentence);
+            testInstance.CountWords();
+            Assert.AreEqual(2, testInstance.Counter);
+        }
+
+        [TestMethod]
+
+        public void CountWords_CountsWordsInSentence_0()
+        {
+            string word = "photo";
+            string sentence = "It has always been a dream of mine to get into photography.";
+            Count testInstance = new Count(word, sentence);
+            testInstance.CountWords();
+            Assert.AreEqual(0, testInstance.Counter);
+        }
 
         [TestMethod]
         public void RemoveSpecialChars_RemovesAllSpecialChars_String()
@@ -38,7 +58,6 @@ namespace WordCounter.Tests
             string result = "hello there frank how are you";
             Assert.AreEqual(result, Count.RemoveSpecialChars(sentence));
         }
-            
 
     }
 }
